e4df2f1abcf3bf9f7559bf1d3b98b2290872bb2e
noomtw <55578381+noomtw@users.noreply.github.com>
2022-03-01 13:39:02 +0700

    Update finalize.sh

--- .jitsi-meet-cfg/jibri/finalize.sh
+++ .jitsi-meet-cfg/jibri/finalize.sh
@@ -6,5 +6,17 @@ echo "This is a dummy finalize script" > /tmp/finalize.out
 echo "The script was invoked with recordings directory $RECORDINGS_DIR." >> /tmp/finalize.out
 echo "You should put any finalize logic (renaming, uploading to a service" >> /tmp/finalize.out
 echo "or storage provider, etc.) in this script" >> /tmp/finalize.out
+echo "am finallize script customize from amang" >> /tmp/finalize.out
+
+
+echo $(mkdir -p /config/recordings/records)
+
+
+_RECORDING_FILENAME=$(find ${RECORDINGS_DIR} -type f -name "*.mp4")
+
+MEETING_ID=$(cat $RECORDINGS_DIR/metadata.json | awk -F'"' '{print $4}' | awk -F'/' '{print $4}')
+$(cp ${_RECORDING_FILENAME} /config/recordings/records/${MEETING_ID}.mp4)
+curl --location --request GET 'http://172.19.0.1:9213/api/onebox/saveonebox/'${MEETING_ID}
+
 
 exit 0
